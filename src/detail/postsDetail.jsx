e09273b2361db06c24ba94903745f2e26b0b2197
akaryweb <yamashita.akary@gmail.com>
2024-10-31 23:03:53 +0900

    6章_課題3

--- src/detail/postsDetail.jsx
+++ src/detail/postsDetail.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useState, useEffect } from 'react'; // useStateとuseEffectをインポート
 import { useParams } from 'react-router-dom';//
-import { posts } from "../data/posts";
+// import { posts } from "../data/posts";
 import styles from './postsDetail.module.css'; // CSS Modulesをインポート
 
 export const PostsDetail = () => {
-  const params = useParams(); /// URLからパラメーターを取得
-  const detailPost = posts.find((postItem) => postItem.id === parseInt(params.id)); // posts配列の各要素（投稿オブジェクト）のプロパティのひとつIDと一致する投稿を検索
+  const { id } = useParams(); // URLから記事のIDを取得
+  const [detailPost, setPost] = useState(null)//初期値が null であるため、「データがまだ存在しない」という状態を明確に表現できる。
+
+  // APIでpostsを取得する処理をuseEffectで実行 by bube
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch(`https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`)//URL内に{id}という文字列をそのまま埋め込んでいるだけになるから、テンプレートリテラルに変更
+      const data = await res.json()
+      setPost(data.post)//APIから取得したデータ（date.post）をpostの状態に設定
+    }
+
+    fetcher()
+
+  }, [id])// 依存関係にある第二引数の配列[id]が変わるたびに再取得refetchする
+
+  // const params = useParams(); /// URLからパラメーターを取得
+  // const detailPost = posts.find((postItem) => postItem.id === parseInt(params.id)); // posts配列の各要素（投稿オブジェクト）のプロパティのひとつIDと一致する投稿を検索
   if (!detailPost) {
     return <div>投稿が見つかりませんでした。</div>; //この処理を入れておくことで、記事が見つからなかった場合のエラーを発生させず、記事が見つからないことを閲覧者に伝えることができる。
   }
--- src/top/postsList.jsx
+++ src/top/postsList.jsx
@@ -1,9 +1,25 @@
 import React from 'react';//React 17以降は、なくてもOK
-import { posts } from '../data/posts'; // 記事データをimport。名前付きimportだから{}が付くよ
+import { useState, useEffect } from 'react'; // useStateとuseEffectをインポート
+// import { posts } from '../data/posts'; // 記事データをimport。名前付きimportだから{}が付くよ
 import styles from './postsList.module.css'; // CSS Modulesをインポート
 import { Link } from 'react-router-dom';//リンクを作成するためにLinkコンポーネントをインポート
 
 export const PostsList = () => {
+
+  const [posts, setPosts] = useState([])//初期値がからの配列[]。APIから投稿データを取得するまで、postsは空の状態ってこと
+
+  // APIでpostsを取得する処理をuseEffectで実行。by bube
+  useEffect(() => {
+    const fetcher = async () => {//fetcherという名前の非同期関数を定義。asyncを関数の前に付けることで、その関数は非同期関数になる
+      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")//暗記
+      const data = await res.json()//暗記
+      setPosts(data.posts)// APIから取得したデータ（data.posts）をpostsの状態に設定
+    }
+
+    fetcher()// fetcher関数を実行すると、APIからデータが取得され、取得したデータがpostsに保存される。
+    //useEffectフック内で定義されているため、コンポーネントがマウントされた時（または依存関係が変更された時）に一度呼び出される。
+  }, [])//第二引数の依存配列を空配列`[]`とすることで、コンポーネントのレンダリング時に一度だけ処理を発火。ページ読込時にデータ取得したい時に用いる手法。by bube
+
   return (
     <div className={styles.post_container}>
       {posts.map((post) => (
